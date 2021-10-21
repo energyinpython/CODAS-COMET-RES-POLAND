3ddbbad665ef3bb570c8a43fbdd26cc3c591e531
energyinpython <92691295+energyinpython@users.noreply.github.com>
2021-10-21 21:03:11 +0200

    Add files via upload

---
+++ software/experiment_tau_selection/additions.py
@@ -0,0 +1,201 @@
+import numpy as np
+import sys
+from scipy.stats import pearsonr
+
+# euclidean distance
+def euclidean_distance(A, B):
+    tmp = np.sum(np.square(A - B))
+    return np.sqrt(tmp)
+
+
+# for hausdorff distance
+def hausdorff(A, B):
+    min_h = np.inf
+    for i in range(len(A)):
+        for j in range(len(B)):
+            d = euclidean_distance(A[i], B[j])
+            if d < min_h:
+                min_h = d
+                min_ind = j
+
+    max_h = -np.inf
+    for i in range(len(A)):
+        d = euclidean_distance(A[i], B[min_ind])
+        if d > max_h:
+            max_h = d
+
+    return max_h
+
+
+# hausdorff distance
+def hausdorff_distance(A, B):
+    ah = hausdorff(A, B)
+    bh = hausdorff(B, A)
+    return max(ah, bh)
+
+
+# correlative distance
+def correlative_distance(A, B):
+    num = np.sum((A - np.mean(A)) * (B - np.mean(B)))
+    denom = np.sqrt(np.sum((A - np.mean(A)) ** 2)) * np.sqrt(np.sum((B - np.mean(B)) ** 2))
+    if denom == 0:
+        denom = sys.float_info.epsilon
+        print('Warning in correlative_distance!')
+    return 1 - (num / denom)
+
+
+# chebyshev distance
+def chebyshev_distance(A, B):
+    max_h = -np.inf
+    for i in range(len(A)):
+        for j in range(len(B)):
+            d = np.abs(A[i] - B[j])
+            if d > max_h:
+                max_h = d
+    return max_h
+
+
+# standarized euclidean distance
+def std_euclidean_distance(A, B):
+    tab_std = np.vstack((A, B))
+    stdv = np.std(tab_std, axis = 0)
+    stdv[stdv == 0] = sys.float_info.epsilon
+    tmp = np.sum(np.square((A - B) / stdv))
+    return np.sqrt(tmp)
+
+
+# cosine distance
+def cosine_distance(A, B):
+    num = np.sum(A * B)
+    denom = (np.sqrt(np.sum(np.square(A)))) * (np.sqrt(np.sum(np.square(B))))
+    if denom == 0:
+        denom = sys.float_info.epsilon
+        print('Warning in cosine_distance!')
+    return 1 - (num / denom)
+
+
+# for CODAS method
+# 0.01 - 0.05 recommended range of tau value
+def psi(x, tau):
+    if np.abs(x) >= tau:
+        return 1
+    else:
+        return 0
+
+
+# linear normalization
+def linear_normalization(matrix, types):
+    ind_profit = np.where(types == 1)
+    ind_cost = np.where(types == -1)
+    nmatrix = np.zeros(np.shape(matrix))
+    nmatrix[:, ind_profit] = matrix[:, ind_profit] / (np.amax(matrix[:, ind_profit], axis = 0))
+    nmatrix[:, ind_cost] = np.amin(matrix[:, ind_cost], axis = 0) / matrix[:, ind_cost]
+    return nmatrix
+
+
+# min-max normalization
+def minmax_normalization(X, criteria_type):
+    x_norm = np.zeros((X.shape[0], X.shape[1]))
+    ind_profit = np.where(criteria_type == 1)
+    ind_cost = np.where(criteria_type == -1)
+
+    x_norm[:, ind_profit] = (X[:, ind_profit] - np.amin(X[:, ind_profit], axis = 0)
+                             ) / (np.amax(X[:, ind_profit], axis = 0) - np.amin(X[:, ind_profit], axis = 0))
+
+    x_norm[:, ind_cost] = (np.amax(X[:, ind_cost], axis = 0) - X[:, ind_cost]
+                           ) / (np.amax(X[:, ind_cost], axis = 0) - np.amin(X[:, ind_cost], axis = 0))
+
+    return x_norm
+
+
+# max normalization
+def max_normalization(X, criteria_type):
+    maximes = np.amax(X, axis=0)
+    ind = np.where(criteria_type == -1)
+    X = X/maximes
+    X[:,ind] = 1-X[:,ind]
+    return X
+
+
+# sum normalization
+def sum_normalization(X, criteria_type):
+    x_norm = np.zeros((X.shape[0], X.shape[1]))
+    ind_profit = np.where(criteria_type == 1)
+    ind_cost = np.where(criteria_type == -1)
+
+    x_norm[:, ind_profit] = X[:, ind_profit] / np.sum(X[:, ind_profit], axis = 0)
+
+    x_norm[:, ind_cost] = (1 / X[:, ind_cost]) / np.sum((1 / X[:, ind_cost]), axis = 0)
+
+    return x_norm
+
+
+# equal weighting
+def mean_weighting(X):
+    N = np.shape(X)[1]
+    return np.ones(N) / N
+
+
+# entropy weighting
+def entropy_weighting(X):
+    # normalization for profit criteria
+    criteria_type = np.ones(np.shape(X)[1])
+    pij = sum_normalization(X, criteria_type)
+
+    Ej = np.zeros(np.shape(pij)[1])
+    for el in range(0, np.shape(pij)[1]):
+        if np.any(pij[:, el] == 0):
+            Ej[el] = 0
+        else:
+            Ej[el] = - np.sum(pij[:, el] * np.log(pij[:, el]))
+
+    Ej = Ej / np.log(X.shape[0])
+
+    wagi = (1 - Ej) / (np.sum(1 - Ej))
+    return wagi
+
+
+# standard deviation weighting
+def std_weighting(X):
+    stdv = np.std(X, axis = 0)
+    return stdv / np.sum(stdv)
+
+
+# CRITIC weighting
+def critic_weighting(X):
+    # normalization for profit criteria
+    criteria_type = np.ones(np.shape(X)[1])
+    x_norm = minmax_normalization(X, criteria_type)
+    std = np.std(x_norm, axis = 0)
+    n = np.shape(x_norm)[1]
+    correlations = np.zeros((n, n))
+    for i in range(0, n):
+        for j in range(0, n):
+            correlations[i, j], _ = pearsonr(x_norm[:, i], x_norm[:, j])
+
+    difference = 1 - correlations
+    suma = np.sum(difference, axis = 0)
+    C = std * suma
+    w = C / (np.sum(C, axis = 0))
+    return w
+
+
+# weighted spearman coefficient rw
+def weighted_spearman(R, Q):
+    N = len(R)
+    denom = N**4 + N**3 - N**2 - N
+    reszta = (N-R+1)+(N-Q+1)
+    suma = 6*sum((R-Q)**2*reszta)
+    rW = 1-(suma/denom)
+    return rW
+
+
+# rank similarity coefficient WS
+def coeff_WS(R, Q):
+    sWS = 0
+    N = len(R)
+    for i in range(N):
+        sWS += 2**(-int(R[i]))*(abs(R[i]-Q[i])/max(abs(R[i] - 1), abs(R[i] - N)))
+    WS = 1 - sWS
+    return WS
+
---
+++ software/experiment_tau_selection/codas_experiments.py
@@ -0,0 +1,124 @@
+import numpy as np
+from itertools import combinations
+import pandas as pd
+import sys
+import os
+import math
+
+from codas import CODAS
+from additions import *
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# plot numeric experiment correlations results
+def plot_boxenplot(data, coeff_name):
+    ax = sns.boxenplot(x = 'variable', y = 'value', hue = r'$\tau$', data = data)
+    ax.tick_params(axis='x', rotation = 90)
+    ax.set_xlabel('Distance')
+    ax.set_ylabel(r'$' + coeff_name + '$')
+    ax.grid(True)
+    ax.set_axisbelow(True)
+    plt.legend(bbox_to_anchor=(0., 1.07, 1., .102), loc='center',
+    ncol=5, borderaxespad=0., edgecolor = 'black', title = r'$\tau$ parameter')
+    plt.tight_layout()
+    plt.savefig('boxenplot_' + coeff_name + '.pdf')
+    plt.show()
+
+
+
+# main
+# list of distance metrics tested
+list_methods_distance = [euclidean_distance,
+                         hausdorff_distance,
+                         correlative_distance,
+                         chebyshev_distance,
+                         std_euclidean_distance,
+                         cosine_distance,
+                         ]
+
+#list of possible pairs of distance metrics tested
+combinations_names = list(combinations(list_methods_distance, 2))
+
+# num of iterations of experiment
+iter = 1000
+# dictionary for r_w correlations
+results_dict_rw = {
+    'variable': [],
+    'value': [],
+    r'$\tau$': []
+    }
+
+# dictionary for WS correlations
+results_dict_WS = {
+    'variable': [],
+    'value': [],
+    r'$\tau$': []
+    }
+
+path = '../../DATASET'
+file = 'dataset_energy_voivodeships_2019.csv'
+pathfile = os.path.join(path, file)
+dane = pd.read_csv(pathfile)
+data = dane.iloc[:, 2:]
+data = data.reset_index()
+data = data.drop(['index'], axis = 1)
+data = data.set_index('Ai')
+
+# determine range of random numbers based on real data to fill decision matrix
+rows = len(data.min())
+cols = 2
+mini = data.min()
+maxi = data.max()
+random_range = np.zeros((rows, cols))
+for i in range(len(mini)):
+    random_range[i, 0] = math.floor(mini[i])
+    random_range[i, 1] = math.ceil(maxi[i])
+
+
+# range of tau threshold parameter
+tau_vec = np.arange(0.01, 0.06, 0.01)
+
+# r - num of rows, c - num of cols, r - 16 - num of alts
+r, c = 16, len(data.min())
+
+for it in range(iter):
+    # generate decision matrix random data
+    matrix = np.zeros((r, c))
+    for i in range(np.shape(random_range)[0]):
+        matrix[:, i] = np.random.uniform(random_range[i, 0], random_range[i, 1], size = r)
+
+    # here are only profit criteria
+    types = np.ones(matrix.shape[1])
+    # determine weights with CRITIC method
+    weights = critic_weighting(matrix)
+    
+    for met_dis in combinations_names:
+        # CODAS
+        for tau_v in tau_vec:
+            pref1 = CODAS(matrix, weights, types, met_dis[0], tau_v)
+            rankingPrep = np.argsort(-pref1)
+            ranking1 = np.argsort(rankingPrep) + 1
+    
+            pref2 = CODAS(matrix, weights, types, met_dis[1], tau_v)
+            rankingPrep = np.argsort(-pref2)
+            ranking2 = np.argsort(rankingPrep) + 1
+
+            coeff_rw = weighted_spearman(ranking1, ranking2)
+            results_dict_rw['variable'].append(met_dis[0].__name__[:4] + r' $ \times $ ' + met_dis[1].__name__[:4])
+            results_dict_rw['value'].append(coeff_rw)
+            results_dict_rw[r'$\tau$'].append(str(tau_v))
+
+            coeffws = coeff_WS(ranking1, ranking2)
+            results_dict_WS['variable'].append(met_dis[0].__name__[:4] + r' $ \times $ ' + met_dis[1].__name__[:4])
+            results_dict_WS['value'].append(coeffws)
+            results_dict_WS[r'$\tau$'].append(str(tau_v))
+
+# transform dictionary with rw correlations data to DataFrame
+results_pd_rw = pd.DataFrame(results_dict_rw)
+# transform dictionary with WS correlations data to DataFrame
+results_pd_WS = pd.DataFrame(results_dict_WS)
+
+# plot correlation results
+plot_boxenplot(results_pd_rw, 'r_w')
+plot_boxenplot(results_pd_WS, 'WS')
